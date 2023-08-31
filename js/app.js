32037c70a81e86159764629c1b216eaa03ae278a
Oskar Zapolski <oskar.zapolski9@gmail.com>
2023-08-31 15:07:12 +0200

    main section2 upgraded and logo added to f1 name teams

--- js/app.js
+++ js/app.js
@@ -1,15 +1,19 @@
 const ul = document.querySelector(".ul-teams");
-console.log(ul);
+const teamLogoDivs = document.querySelectorAll("[data-key]");
+console.log(teamLogoDivs);
 
 fetch("http://ergast.com/api/f1/2023/constructors.json")
   .then((response) => response.json())
   .then((data) => {
     for (let i = 0; i < data.MRData.ConstructorTable.Constructors.length; i++) {
-      const li = document.createElement("li");
-
-      li.textContent = data.MRData.ConstructorTable.Constructors[i].name;
-      li.className = "constructors-name";
-      ul.appendChild(li);
+      const p = document.createElement("p");
+      const img = document.createElement("img");
+      img.src = `img/teamLogo${i}.jpg`;
+      img.className = "logo-team-img";
+      p.textContent = data.MRData.ConstructorTable.Constructors[i].name;
+      p.className = "constructors-name";
+      teamLogoDivs[i].appendChild(img);
+      teamLogoDivs[i].appendChild(p);
     }
   })
   .catch((err) => console.log(err));
