c6f9a43e5c49ee919ebfbd7e61596c0efae441e7
Oskar Zapolski <oskar.zapolski9@gmail.com>
2023-09-03 14:45:54 +0200

    main slider arrows and dots working

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 const teamLogoDivs = document.querySelectorAll("[data-key]");
 const LogoA = document.querySelectorAll(".img-logo-link");
 const imgsSlider = [...document.querySelectorAll(".img-slider")];
-const dots = document.querySelectorAll(".circle");
+const dots = [...document.querySelectorAll(".circle")];
 const arrowLeft = document.querySelector(".arrow-left");
 const arrowRight = document.querySelector(".arrow-right");
 
@@ -23,18 +23,57 @@ fetch("http://ergast.com/api/f1/2023/constructors.json")
   })
   .catch((err) => console.log(err));
 
-function nextSlide() {
-  let activeSlide = imgsSlider.findIndex((img) => {
+function findActives() {
+  let activeSLide = imgsSlider.findIndex((img) => {
     return img.classList.contains("active");
   });
+  let activeDot = dots.findIndex((dot) => dot.classList.contains("active-dot"));
+  return [activeSLide, activeDot];
+}
+function removeActive(activeSlide, activeDot) {
   imgsSlider[activeSlide].classList.remove("active");
-
-  if (activeSlide < imgsSlider.length - 1) {
-    activeSlide += 1;
-  } else {
-    activeSlide = 0;
-  }
-  imgsSlider[activeSlide].classList.add("active");
+  dots[activeDot].classList.remove("active-dot");
 }
 
+function nextSlide() {
+  let activeSlide = findActives()[0];
+  let activeDot = findActives()[1];
+  removeActive(activeSlide, activeDot);
+  if (activeSlide < imgsSlider.length - 1) {
+    activeSlide += 1;
+    activeDot += 1;
+  } else {
+    activeSlide = 0;
+    activeDot = 0;
+  }
+  imgsSlider[activeSlide].classList.add("active");
+  dots[activeDot].classList.add("active-dot");
+}
+function prevSlide() {
+  let activeSlide = findActives()[0];
+  let activeDot = findActives()[1];
+  removeActive(activeSlide, activeDot);
+  if (activeSlide > 0) {
+    activeSlide -= 1;
+    activeDot -= 1;
+  } else {
+    activeSlide = imgsSlider.length - 1;
+    activeDot = dots.length - 1;
+  }
+  imgsSlider[activeSlide].classList.add("active");
+  dots[activeDot].classList.add("active-dot");
+}
+
+dots.forEach((dot, i) => {
+  dot.addEventListener("click", () => {
+    let activeSlide = findActives()[0];
+    let activeDot = findActives()[1];
+    removeActive(activeSlide, activeDot);
+
+    dot.classList.add("active-dot");
+    imgsSlider[i].classList.add("active");
+  });
+});
+
 arrowRight.addEventListener("click", nextSlide);
+arrowLeft.addEventListener("click", prevSlide);
